504c4a90c36f70b08d4d5e32efc7de64c91bc24c
Varunkumar S H <130498799+varunkumar1235@users.noreply.github.com>
2024-01-04 21:10:38 +0530

    Update main.c
    Entered comments for case 1 and 2

--- main.c
+++ main.c
@@ -19,21 +19,54 @@ int main()
     while(1)
     {
         printf("About what are you looking for :\n");
-        printf("1.Home.\t\t\t2.Plot.\t\t\t3.Commercial.\n");
+        printf("1.Plot.\t\t\t2.Home.\t\t\t3.Commercial.\n");
         printf("Enter choice: ");
         scanf("%d", &ch);
 
         switch (ch)
         {
         case 1:
-            printf("1.Find home.\t\t2.Check availability of home at a particular location.\t\t3.List ");
-            
+            //print all ready-to-move-in plots(print 3 only, functional navigation buttons required)
+            //print all investment type plots(print 3 only, sort by rate of average growth)
+
+            //take choice input 
+            //case 1 : ready to move in
+                //take inputs - locality,budget(min-max input),size
+                //print output for all the matching parameters(navigation button)(single source shortest path)
+                //take input for prime facility
+                //print output according to prime facility(closest,navigation button)(sorting)
+                //recommendation system
+            //case 2 : investment
+                //take inputs - locality,budget(min-max input),size
+                //print output for all the matching parameters(navigation button)(sorted by average increase)
+                //recommendation system
             break;
         case 2:
+            //print all buy-able houses(print 3 only, functional navigation buttons required)
+            //print all rent-able houses(print 3 only, functional navigation buttons required)
+            //print all both-able houses(print 3 only, functional navigation buttons required)
+            //take input on rent or buy(chooses from rent and both, or buy and both)
 
+            //print all single-standing type houses(print 3 only, functional navigation buttons required)
+            //print all flat houses(print 3 only, functional navigation buttons required)
+            //take input on single or flat(case 1 or 2)
+
+            //case 1 : single standing
+                //take inputs - locality,budget(min-max input),no_of_bedrooms,size
+                //print output for all the matching parameters(navigation button)(single source shortest path)
+                //take input for prime facility
+                //print output according to prime facility(closest,navigation button)(sorting)
+                //recommendation system
+
+            //case 2 : flat
+                //take inputs - locality,budget(min-max input),no_of_bedrooms,size
+                //print output for all the matching parameters(navigation button)(single source shortest path)
+                //take input for prime facility
+                //print output according to prime facility(closest,navigation button)(sorting)
+                //recommendation system
             break;
         case 3:
-
+            
             break;
         default:
             printf("The program has been terminated.\n \t\t\tThanking you.");
