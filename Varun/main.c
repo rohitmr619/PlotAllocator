2982cd996662fa2d887214356b3d15cde58990da
Varunkumar S H <130498799+varunkumar1235@users.noreply.github.com>
2024-01-21 17:08:59 +0530

    Update main.c

--- Varun/main.c
+++ Varun/main.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+#include"functions.c"
 
+int main()
+{
+    //Integer declared to manage loops in main()
+    int i;
+/*----------------------1.Shortest Path----------------------*/
+    //Reads facilty data into array of structures
+    facility_pointer = fopen("facilities1.txt","r+");
+    read_facilities();         
+    fseek(facility_pointer,0,0);                        
+
+    //Populates road-connection matrix
+    populate_connection_matrix();
+
+    //Populates cost matrix
+    populate_distance_matrix();                             
+
+    //Displays road-connection matrix
+    //display_connection_matrix();    
+
+    //Displays cost matrix
+    //display_distance_matrix();                              
+
+    /*dijkstras() function called
+    First parameter is the no. of vertices or nodes
+    Second parameter is source which is 0 in this case*/
+    array_dijkstra(20,0);     
+/*-----------------------------------------------------------*/
+
+/*----------------------V----------------------*/                                   
+}
